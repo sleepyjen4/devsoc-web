0a131f6f5b1fbf0181ca2d409c209e32f80b8ef8
jleo0359 <jleo0359@uni.sydney.edu.au>
2026-01-22 01:00:46 +1100

    feat: simple barrel movement and horse jumping

---
+++ script.js
@@ -0,0 +1,69 @@
+const horse = document.getElementById("horse");
+const barrel = document.getElementById("barrel");
+
+const GROUND_HEIGHT = 40;
+let isJumping = false;
+
+document.addEventListener("keydown", function () {
+    if (!isJumping) {
+        jump();
+    }
+});
+
+function jump() {
+    let position = 25;
+    isJumping = true;
+
+    const upInterval = setInterval(() => {
+        if (position >= GROUND_HEIGHT + 80) {
+          clearInterval(upInterval);
+    
+          const downInterval = setInterval(() => {
+            if (position < GROUND_HEIGHT) {
+              position = GROUND_HEIGHT;
+              horse.style.bottom = position + "px";
+              clearInterval(downInterval);
+              isJumping = false;
+            }
+            position -= 5;
+            horse.style.bottom = position + "px";
+          }, 20);
+        }
+    
+        position += 5;
+        horse.style.bottom = position + "px";
+      }, 20);
+}
+
+function moveBarrel() {
+  let barrelPosition = 580;
+
+  setInterval(() => {
+    if (barrelPosition < -20) {
+        barrelPosition = 580;
+    }
+
+    barrelPosition -= 5;
+    barrel.style.left = barrelPosition + "px";
+  }, 20);
+}
+
+moveBarrel();
+
+setInterval(() => {
+    const horseBottom = parseInt(
+        window.getComputedStyle(horse).getPropertyValue("bottom")
+    );
+    const barrelLeft = parseInt(
+        window.getComputedStyle(barrel).getPropertyValue("left")
+    );
+  
+    if (
+        barrelLeft < 110 &&
+        barrelLeft > 50 &&
+        horseBottom <= GROUND_HEIGHT + 10
+    ) {
+      location.reload();
+    }
+  }, 10);
+  
